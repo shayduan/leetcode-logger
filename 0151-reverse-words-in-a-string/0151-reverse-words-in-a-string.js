53d1fd62b8b1ffc931d6733f36ff38ce515a4766
Shay <59212994+shayduan@users.noreply.github.com>
2026-05-02 22:05:53 -0700

    Time: 5 ms (18.88%), Space: 54 MB (98.38%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -2,7 +2,23 @@
  * @param {string} s
  * @return {string}
  */
+
 var reverseWords = function(s) {
+    s = s.trim(); // remove spaces in the beginning and the end
+    let len = s.length;
+    let i = len - 1, j = len - 1;
+    let res = [];
+    while (i >= 0) {
+        while (i >= 0 && s[i] !== ' ') i--; // find the first space from right to left
+        res.push(s.slice(i + 1, j + 1));
+        while (i >= 0 && s[i] === ' ') i--; // skip spaces in between
+        j = i;
+    }
+    return res.join(' ');
+}
+
+// left to right + array unshift, O(n)
+var reverseWords1 = function(s) {
     const space = ' ';
     let len = s.length;
     let array = [];
