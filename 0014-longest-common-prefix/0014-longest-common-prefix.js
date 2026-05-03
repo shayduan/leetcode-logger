ecd1dabb6a74b9633b98e226e58807f647f44b8f
Shay <59212994+shayduan@users.noreply.github.com>
2026-05-02 22:11:50 -0700

    Time: 0 ms (100%), Space: 53 MB (96.23%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+// O(nlogn)
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+    strs.sort();
+    let first = strs[0];
+    let last = strs[strs.length - 1];
+    let i = 0;
+    while (i < first.length && first[i] === last[i]) i++;
+    return first.slice(0, i);
+};
