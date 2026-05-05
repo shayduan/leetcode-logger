08d184298d26aa602b8df72c9c3df185bc2fa472
Shay <59212994+shayduan@users.noreply.github.com>
2026-05-04 22:49:49 -0700

    Time: 3 ms (85.95%), Space: 58.3 MB (49.33%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase();
+    s = s.replace(/[^a-z0-9]/g, '');
+    return s === s.split('').reverse().join('');
+};
