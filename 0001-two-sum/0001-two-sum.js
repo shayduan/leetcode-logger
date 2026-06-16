1a5e7e2b7935f7fbf425c26c92a34a3548e08da0
Shay <59212994+shayduan@users.noreply.github.com>
2026-06-15 20:04:15 -0700

    Time: 4 ms (50.86%), Space: 55.6 MB (27.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,10 +6,9 @@
 var twoSum = function(nums, target) {
     let map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            return [map.get(nums[i]), i];
-        }
-        map.set(target - nums[i], i);
+        let complement = target - nums[i];
+        if (map.has(complement)) return [map.get(complement), i];
+        map.set(nums[i], i);
     }
     return [-1, -1]; // should not reach here
 };
