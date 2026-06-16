84953a6e0cc87ee2010b7c9ff07e39747a15cbbb
Shay <59212994+shayduan@users.noreply.github.com>
2026-06-15 23:11:09 -0700

    Time: 5 ms (77.92%), Space: 69.6 MB (72.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,24 +4,24 @@
  */
 
 // O(n) O(1) store prefix and suffix in ans array
-var productExceptSelf1 = function(nums) {
+var productExceptSelf = function(nums) {
     let n = nums.length;
     let ans = new Array(n).fill(1);
-    let prod = 1;
+    // prefix
     for (let i = 1; i < n; i++) {
-        prod *= nums[i - 1];
-        ans[i] = prod;
+        ans[i] = ans[i - 1] * nums[i - 1];
     }
-    prod = 1;
+    // prefix * suffix
+    let suffix = 1;
     for (let i = n - 2; i >= 0; i--) {
-        prod *= nums[i + 1];
-        ans[i] *= prod;
+        suffix *= nums[i + 1];
+        ans[i] *= suffix;
     }
     return ans;
 };
 
 // O(n) O(n), ans[i] = prefix[i] * suffix[i]
-var productExceptSelf = function(nums) {
+var productExceptSelf0 = function(nums) {
     let n = nums.length;
     let prefix = new Array(n).fill(1);
     let suffix = new Array(n).fill(1);
