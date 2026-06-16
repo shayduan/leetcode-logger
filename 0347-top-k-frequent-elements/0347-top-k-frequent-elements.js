aa8261f6e902ebf66cf3ab7bacb86aa171a66093
Shay <59212994+shayduan@users.noreply.github.com>
2026-06-15 22:16:42 -0700

    Time: 24 ms (16.26%), Space: 63.3 MB (19.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -3,7 +3,26 @@
  * @param {number} k
  * @return {number[]}
  */
+
+// heap
 var topKFrequent = function(nums, k) {
+    let map = new Map();
+
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+    
+    let heap = new MinPriorityQueue({ compare: (a, b) => a[1] - b[1]});
+    for (let entry of map) {
+        heap.enqueue(entry);
+        if (heap.size() > k) heap.dequeue();
+    }
+
+    return heap.toArray().map(e => e[0]);
+};
+
+// bucket sort, O(n) O(n)
+var topKFrequent0 = function(nums, k) {
     let map = new Map();
     let n = nums.length;
     let bucket = new Array(n + 1).fill(0).map(() => []);
