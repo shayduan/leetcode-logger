5b8ee7944afb7eafdcc382f2af2643f93fe7217b
Shay <59212994+shayduan@users.noreply.github.com>
2026-06-17 00:24:02 -0700

    Time: 13 ms (85.82%), Space: 57.9 MB (22.66%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+
+// sliding window [l, r], windowSize = r - l + 1
+// windowSize - highestFrequency <= k, valid substring
+// windowSize <= k + highestFrequency
+// goal -> largest window size -> largest highestFrequency
+var characterReplacement = function(s, k) {
+    let freq = new Array(26).fill(0);
+    let [l, r] = [0, 0];
+    let maxFreq = 0;
+    let maxLen = 0;
+    let n = s.length;
+    const A_ASCII = 'A'.charCodeAt();
+
+    while (r < n) {
+        // update freq and maxFreq
+        freq[s[r].charCodeAt() - A_ASCII]++;
+        maxFreq = Math.max(maxFreq, freq[s[r].charCodeAt() - A_ASCII]);
+        // check if not valid (potentially move l)
+        if (r - l + 1 - maxFreq > k) {
+            freq[s[l].charCodeAt() - A_ASCII]--;
+            l++;
+        }
+        // check if window size is larger
+        maxLen = Math.max(maxLen, r - l + 1);
+        // move r
+        r++;
+    }
+
+    return maxLen;
+};
