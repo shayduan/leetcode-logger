955ee31a70536a5e8cdbeaa7bc3becdc9c332aa1
Shay <59212994+shayduan@users.noreply.github.com>
2026-06-29 23:51:16 -0700

    Time: 0 ms (100%), Space: 58.3 MB (27.23%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let [l ,r] = [0, nums.length - 1];
+    
+    while (l <= r) {
+        let mid = (l + r) >> 1;
+        if (nums[mid] === target) return mid;
+
+        if (target > nums[mid]) l = mid + 1;
+        else r = mid - 1;
+    }
+    return -1;
+};
