47d08bdf57c5cda03331d09024aa2a4e32f1860b
Shay <59212994+shayduan@users.noreply.github.com>
2026-07-04 02:39:25 -0700

    Time: 0 ms (100%), Space: 53.9 MB (24.14%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// dp[n] - the maximum # of money we can rob from n houses
+// dp[n] = Math.max(dp[n - 1], dp[n - 2] + nums[n])
+// O(n), O(n) -> O(1)
+var rob = function(nums) {
+    const n = nums.length;
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 0;
+    dp[1] = nums[0];
+    for (let i = 2; i <= n; i++) {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+    }
+    return dp[n];
+};
