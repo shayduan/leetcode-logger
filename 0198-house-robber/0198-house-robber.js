87f6c26d9c846369218077df80775a17b2540bba
Shay <59212994+shayduan@users.noreply.github.com>
2026-07-04 02:41:10 -0700

    Time: 0 ms (100%), Space: 53.8 MB (40.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -8,11 +8,18 @@
 // O(n), O(n) -> O(1)
 var rob = function(nums) {
     const n = nums.length;
-    let dp = new Array(n + 1).fill(0);
-    dp[0] = 0;
-    dp[1] = nums[0];
+    // let dp = new Array(n + 1).fill(0);
+    // dp[0] = 0;
+    // dp[1] = nums[0];
+    // for (let i = 2; i <= n; i++) {
+    //     dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+    // }
+    // return dp[n];
+
+    let prev2 = 0, prev = nums[0];
     for (let i = 2; i <= n; i++) {
-        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        let cur = Math.max(prev, prev2 + nums[i - 1]);
+        [prev2, prev] = [prev, cur];
     }
-    return dp[n];
+    return prev;
 };
