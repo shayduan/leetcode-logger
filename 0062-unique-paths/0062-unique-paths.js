35593f3016d6a75deaf88fb499f70e6f81faadc3
Shay <59212994+shayduan@users.noreply.github.com>
2026-07-04 23:02:52 -0700

    Time: 0 ms (100%), Space: 54.7 MB (21.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -9,8 +9,9 @@
 // dp[i][j] = dp[i-1][j] if grid[i-1][j] is valid + dp[i][j-1] if grid[i][j-1] is valid
 // O(mn) O(mn) -> O(n)
 var uniquePaths = function(m, n) {
-    const isValid = (i, j) => i >= 0 && i < m && j >= 0 && j < n;
+    // const isValid = (i, j) => i >= 0 && i < m && j >= 0 && j < n;
     // let dp = Array.from({length: m}, () => new Array(n).fill(0));
+    if (m < n) [m, n] = [n, m];
     let cur = new Array(n).fill(1);
 
     // dp[0][0] = 1;
