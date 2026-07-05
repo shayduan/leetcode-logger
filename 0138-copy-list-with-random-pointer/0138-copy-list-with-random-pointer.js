a0fdc6e1e0264448fe16d08cf157d93771f0ac9c
Shay <59212994+shayduan@users.noreply.github.com>
2026-07-05 01:07:15 -0700

    Time: 59 ms (5.03%), Space: 56.4 MB (12.13%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
@@ -13,7 +13,28 @@
  */
 var copyRandomList = function(head) {
     if (!head) return head;
-    let map = new Map(); // index -> _Node
+    let map = new Map(); // old node -> new node
+    map.set(null, null);
+    
+    let cur = head;
+    while (cur) {
+        map.set(cur, new _Node(cur.val, null, null));
+        cur = cur.next;
+    }
+
+    cur = head;
+    while (cur) {
+        map.get(cur).next = map.get(cur.next);
+        map.get(cur).random = map.get(cur.random);
+        cur = cur.next;
+    }
+
+    return map.get(head);
+}
+
+var copyRandomList_self = function(head) {
+    if (!head) return head;
+    let map = new Map(); // old node -> new node
     let originHead = head;
     let newHead = new _Node(head.val, head.next, null);
     let dummy = new _Node(0, newHead, null);
