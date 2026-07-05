3947d246e28ed50b1a3cd33c8ed66b6e770302b3
Shay <59212994+shayduan@users.noreply.github.com>
2026-07-05 03:03:33 -0700

    Time: 86 ms (96.33%), Space: 111.9 MB (12.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -1,9 +1,5 @@
 class Node {
-    prev; // Node
-    next; // Node
-    key;
-    value;
-    constructor(key, value, prev = null, next = null) {
+    constructor(key = -1, value = -1, prev = null, next = null) {
         this.key = key;
         this.value = value;
         this.prev = prev;
@@ -11,88 +7,76 @@ class Node {
     }
 }
 
-class LRUCache {
-    cap; // capacity
-    cache; // hashmap
-    LRU; // least recently used -> Leftest dummy node
-    MRU; // most recently used -> Rightest dummy node
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.capacity = capacity;
+    this.map = new Map(); // key -> node
+    this.MRU = new Node(); // most recently used, dummy node
+    this.LRU = new Node(); // least recently used, dummy node
+    
+    this.MRU.next = this.LRU;
+    this.LRU.prev = this.MRU;
+};
 
-    constructor(capacity) {
-        this.cap = capacity;
-        this.cache = new Map();
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (!this.map.has(key)) return -1;
 
-        this.LRU = new Node(-1, -1);
-        this.MRU = new Node(-1, -1);
-        this.LRU.next = this.MRU;
-        this.MRU.prev = this.LRU;
+    let node = this.map.get(key);
+    this.remove(node);
+    this.addToMRU(node);
+
+    return node.value;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    let node;
+    if (this.map.has(key)) {
+        // if key exist, update the value
+        node = this.map.get(key);
+        node.value = value;
+        this.remove(node);
     }
-
-    get(key) {
-        if (this.cache.has(key)) {
-            let node = this.cache.get(key);
-            // move to the MRU
-            this.#remove(node);
-            this.#insert(node);
-            return node.value;
-        }
-        return -1;
+    else {
+        // if not exist, add key-value
+        node = new Node(key, value);
+        this.map.set(key, node);
     }
-
-    put(key, value) {
-        if (this.cache.has(key)) {
-            let node = this.cache.get(key);
-            node.value = value;
-            this.#remove(node);
-            this.#insert(node);
-        } else {
-            let node = new Node(key, value);
-            this.cache.set(key, node);
-            this.#insert(node);
-            if (this.cache.size > this.cap) {
-                this.cache.delete(this.LRU.next.key);
-                this.#remove(this.LRU.next);
-            }
-        }
+    // move current node to MRU
+    this.addToMRU(node);
+    // if node # > capacity, remove the LRU
+    if (this.map.size > this.capacity) {
+        let lruNode = this.LRU.prev;
+        this.remove(lruNode);
+        this.map.delete(lruNode.key);
     }
+};
 
-    // remove from linked list
-    #remove(node) {
-        let [prev, next] = [node.prev, node.next];
-        prev.next = next;
-        next.prev = prev;
-    }
-
-    // insert to the MRU side in the linked list
-    #insert(node) {
-        let prev = this.MRU.prev;
-        [prev.next, this.MRU.prev] = [node, node];
-        [node.prev, node.next] = [prev, this.MRU];
-    }
+LRUCache.prototype.remove = function(node) {
+    // prev -> node -> next
+    let [prev, next] = [node.prev, node.next];
+    prev.next = next;
+    next.prev = prev;
+    return node;
 }
 
-// /**
-//  * @param {number} capacity
-//  */
-// var LRUCache = function(capacity) {
-    
-// };
-
-// /** 
-//  * @param {number} key
-//  * @return {number}
-//  */
-// LRUCache.prototype.get = function(key) {
-    
-// };
-
-// /** 
-//  * @param {number} key 
-//  * @param {number} value
-//  * @return {void}
-//  */
-// LRUCache.prototype.put = function(key, value) {
-    
-// };
+LRUCache.prototype.addToMRU = function(node) {
+    // this.MRU -> node -> next
+    let next = this.MRU.next;
+    this.MRU.next = node;
+    [node.prev, node.next] = [this.MRU, next];
+    next.prev = node;
+}
 
 /** 
  * Your LRUCache object will be instantiated and called as such:
@@ -100,3 +84,63 @@ class LRUCache {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+
+
+// class LRUCache {
+//     cap; // capacity
+//     cache; // hashmap
+//     LRU; // least recently used -> Leftest dummy node
+//     MRU; // most recently used -> Rightest dummy node
+
+//     constructor(capacity) {
+//         this.cap = capacity;
+//         this.cache = new Map();
+
+//         this.LRU = new Node(-1, -1);
+//         this.MRU = new Node(-1, -1);
+//         this.LRU.next = this.MRU;
+//         this.MRU.prev = this.LRU;
+//     }
+
+//     get(key) {
+//         if (this.cache.has(key)) {
+//             let node = this.cache.get(key);
+//             // move to the MRU
+//             this.#remove(node);
+//             this.#insert(node);
+//             return node.value;
+//         }
+//         return -1;
+//     }
+
+//     put(key, value) {
+//         if (this.cache.has(key)) {
+//             let node = this.cache.get(key);
+//             node.value = value;
+//             this.#remove(node);
+//             this.#insert(node);
+//         } else {
+//             let node = new Node(key, value);
+//             this.cache.set(key, node);
+//             this.#insert(node);
+//             if (this.cache.size > this.cap) {
+//                 this.cache.delete(this.LRU.next.key);
+//                 this.#remove(this.LRU.next);
+//             }
+//         }
+//     }
+
+//     // remove from linked list
+//     #remove(node) {
+//         let [prev, next] = [node.prev, node.next];
+//         prev.next = next;
+//         next.prev = prev;
+//     }
+
+//     // insert to the MRU side in the linked list
+//     #insert(node) {
+//         let prev = this.MRU.prev;
+//         [prev.next, this.MRU.prev] = [node, node];
+//         [node.prev, node.next] = [prev, this.MRU];
+//     }
+// }
