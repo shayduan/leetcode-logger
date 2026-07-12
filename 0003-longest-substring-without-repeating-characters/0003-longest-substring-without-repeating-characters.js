ded2fee385640878c691888eba1b82a8fc01156b
Shay <59212994+shayduan@users.noreply.github.com>
2026-07-12 01:16:19 -0700

    Time: 3 ms (94.31%), Space: 56.4 MB (86.11%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,8 +3,23 @@
  * @return {number}
  */
 
-// O(n) O(n), sliding window + map
+
 var lengthOfLongestSubstring = function(s) {
+    let l = 0, r = 0;
+    let max = 0;
+    let map = new Map();
+    while (r < s.length) {
+        let cur = s[r];
+        if (map.has(cur)) l = Math.max(l, map.get(cur) + 1);
+        map.set(cur, r);
+        max = Math.max(max, r - l + 1);
+        r++;
+    }
+    return max;
+}
+
+// O(n) O(n), sliding window + map
+var lengthOfLongestSubstring0 = function(s) {
     let map = new Map(); // char -> index
     let l = 0, r = 0;
     let maxlen = 0;
