183c5d34d0eac2c5b26fb9fa6d684683d0bf2a61
Shay <59212994+shayduan@users.noreply.github.com>
2026-07-12 02:12:16 -0700

    Time: 5 ms (99.11%), Space: 57 MB (43.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,11 +4,55 @@
  * @return {number}
  */
 
+var characterReplacement = function(s, k) {
+    let l = 0, r = 0;
+    let count = new Array(26).fill(0);
+    let maxLen = 0, maxCount = 0;
+    while (r < s.length) {
+        let index = s[r].charCodeAt() - 'A'.charCodeAt();
+        count[index]++;
+        maxCount = Math.max(maxCount, count[index]);
+
+        if (r - l + 1 - maxCount > k) {
+            count[s[l].charCodeAt() - 'A'.charCodeAt()]--;
+            l++;
+        }
+        maxLen = Math.max(maxLen, r - l + 1);
+        r++;
+    }
+    return maxLen;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // sliding window [l, r], windowSize = r - l + 1
 // windowSize - highestFrequency <= k, valid substring
 // windowSize <= k + highestFrequency
 // goal -> largest window size -> largest highestFrequency
-var characterReplacement = function(s, k) {
+var characterReplacement0 = function(s, k) {
     let freq = new Array(26).fill(0);
     let [l, r] = [0, 0];
     let maxFreq = 0;
